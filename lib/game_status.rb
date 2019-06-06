2202b554066c7df7ed817cc6447d012ceb132278
Tracie Masek <traciemasek@gmail.com>
2019-06-06 11:19:49 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,7 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.find do |win_combo|
     board[win_combo[0]] == board[win_combo[1]] && board[win_combo[1]] == board[win_combo[2]] && position_taken?(board, win_combo[0])
-  end #end of detect block
+  end 
 end
 
 def full?(board)
