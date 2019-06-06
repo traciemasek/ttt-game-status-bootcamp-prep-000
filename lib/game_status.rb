10a621cd8915752771d36cade4e3acbb24aad6df
Tracie Masek <traciemasek@gmail.com>
2019-06-06 10:16:08 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,6 +18,10 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
     win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    
+    position_1 = board[win_index_1]
     
   end #end of each block
 end
