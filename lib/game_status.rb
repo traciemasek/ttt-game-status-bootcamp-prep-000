c07887eaf12868f9afe313e4bf2f42d350d0c683
Tracie Masek <traciemasek@gmail.com>
2019-06-06 10:16:23 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -22,6 +22,8 @@ def won?(board)
     win_index_3 = win_combination[2]
     
     position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
     
   end #end of each block
 end
